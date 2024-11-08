882cdd6b667e24f9c766250ab36b6522f7e5d925
LovepreetSingh12 <luvprits33@gmail.com>
2024-11-08 22:10:32 +0530

    Udhghaatan

---
+++ src/test/java/com/n117/breaditt/BreadittApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n117.breaditt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadittApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
