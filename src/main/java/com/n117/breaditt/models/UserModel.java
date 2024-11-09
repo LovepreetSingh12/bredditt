b424755cfbc01b6dc12a6282b2b9a00e61a1ae8f
Manas Mishra <manasmishra2520@gmail.com>
2024-11-09 23:01:40 +0530

    added more parameters to UserModel

--- src/main/java/com/n117/breaditt/models/UserModel.java
+++ src/main/java/com/n117/breaditt/models/UserModel.java
@@ -3,6 +3,8 @@ package com.n117.breaditt.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document(collection = "users")
 public class UserModel {
 
@@ -11,15 +13,20 @@ public class UserModel {
     private String userName;
     private String name;
     private String about;
-    private int age;
+    private Integer age; // TODO : Integer or int?
     private String email;
-    private String phoneNumber;
+    private String phoneNumber; // TODO : String or int? (to include international codes like +34)?
+    private String profilePictureUrl;
+    private List<String> createdPosts;
+    private List<String> savedPosts;
+    private List<String> createdComments;
+    private List<String> savedComments;
+    private List<String> socialMediaLinks;
+    private List<String> subredditsJoined;
+    private List<String> blockedUsers;
 
-    //constructors
-    public UserModel() {
-
-    }
-    public UserModel(String userId, String userName, String name, String about, int age, String email, String phoneNumber) {
+    // Constructor
+    public UserModel(String userId, String userName, String name, String about, Integer age, String email, String phoneNumber, String profilePictureUrl, List<String> savedPosts, List<String> createdPosts, List<String> createdComments, List<String> savedComments, List<String> socialMediaLinks, List<String> subredditsJoined, List<String> blockedUsers) {
         this.userId = userId;
         this.userName = userName;
         this.name = name;
@@ -27,9 +34,17 @@ public class UserModel {
         this.age = age;
         this.email = email;
         this.phoneNumber = phoneNumber;
+        this.profilePictureUrl = profilePictureUrl;
+        this.savedPosts = savedPosts;
+        this.createdPosts = createdPosts;
+        this.createdComments = createdComments;
+        this.savedComments = savedComments;
+        this.socialMediaLinks = socialMediaLinks;
+        this.subredditsJoined = subredditsJoined;
+        this.blockedUsers = blockedUsers;
     }
 
-    //Getter-Setters
+    // Getters-Setters
     public String getUserId() {
         return userId;
     }
@@ -62,11 +77,11 @@ public class UserModel {
         this.about = about;
     }
 
-    public int getAge() {
+    public Integer getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
@@ -84,5 +99,69 @@ public class UserModel {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+
+    public List<String> getCreatedPosts() {
+        return createdPosts;
+    }
+
+    public void setCreatedPosts(List<String> createdPosts) {
+        this.createdPosts = createdPosts;
+    }
+
+    public List<String> getSavedPosts() {
+        return savedPosts;
+    }
+
+    public void setSavedPosts(List<String> savedPosts) {
+        this.savedPosts = savedPosts;
+    }
+
+    public List<String> getCreatedComments() {
+        return createdComments;
+    }
+
+    public void setCreatedComments(List<String> createdComments) {
+        this.createdComments = createdComments;
+    }
+
+    public List<String> getSavedComments() {
+        return savedComments;
+    }
+
+    public void setSavedComments(List<String> savedComments) {
+        this.savedComments = savedComments;
+    }
+
+    public List<String> getSocialMediaLinks() {
+        return socialMediaLinks;
+    }
+
+    public void setSocialMediaLinks(List<String> socialMediaLinks) {
+        this.socialMediaLinks = socialMediaLinks;
+    }
+
+    public List<String> getSubredditsJoined() {
+        return subredditsJoined;
+    }
+
+    public void setSubredditsJoined(List<String> subredditsJoined) {
+        this.subredditsJoined = subredditsJoined;
+    }
+
+    public List<String> getBlockedUsers() {
+        return blockedUsers;
+    }
+
+    public void setBlockedUsers(List<String> blockedUsers) {
+        this.blockedUsers = blockedUsers;
     }
 }
